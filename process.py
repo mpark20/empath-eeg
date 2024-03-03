ed0923777459617ac1ef53f70c3d31366fa46d46
s-brumleyma <54920660+madbrum@users.noreply.github.com>
2024-03-03 10:56:31 -0800

    AHHHHHH data sampling idk if this works

--- process.py
+++ process.py
@@ -1,31 +1,63 @@
-# import tensorflow as tf
-# import keras
 #from .preprocessing.dataStreaming import main
-#i think we need to add a send/read data function to this dataStreaming file and call it here
+#placeholder for preprocessing + read raw data functions 
 
-#model = tf.keras.models.load_model('classifier.keras')
 import pickle
+import random
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
 
 def process_prompt(role, text, label):
+    '''
+    Prepends emotion state to prompt if role is user
+    '''
     if role == "assistant":
         return text
     return ''.join([r"[[[", label, r"]]] ", text])
 
-def predict_emotion(data):
+def predict_emotion():
     '''
-    Returns the predicted emotion (int) given a row of data
-    0=negative, 1=neutral, 2=positive
-
-    data: 1d ndarray of features
-    '''    
+    Returns the predicted emotion given a row of data
+    '''   
+    # TODO: read in raw data from other class 
+    data = []
     # TODO : preprocess into binned data
-    input = data.reshape(-1, 1) # reshape into 2d array
-    with open('models/classifierV0', 'rb') as f:
+    if data == []:
+        datas = read_sample()
+        data = datas[random.choice(range(datas.shape[0]))]
+    input = data.reshape(1, -1)
+    with open('../models/classifierV0', 'rb') as f:
         loaded_rf = pickle.load(f)
         pred = loaded_rf.predict(input)  
     return pred
 
-def read():
-    #how to read in data from eeg/dataStreaming file?
-    print("placeholder")
-    return None
+def read_sample():
+    data = pd.read_csv('./data/emotions.csv')
+    label_mapping = {'NEGATIVE': 0, 'NEUTRAL': 1, 'POSITIVE': 2}
+    nan_count = np.sum(data.isnull(), axis = 0)
+    nan_cols = nan_count != 0
+    #splits the columns with null values into numerical and categorical dfs
+    col_w_nan = data.columns[nan_count != 0]
+    num_columns = [col for col in col_w_nan if data[col].dtype == 'int64' or data[col].dtype == 'float64']
+    for col in num_columns:
+        data = replaceNullNum(data, col)
+    data.dropna(inplace = True)
+    _, X_test, _, _ = preprocess_data(data, label_mapping)
+    return X_test
+
+def replaceNullNum(data, col):
+  mean = data[col].mean()
+  data[col].fillna(value=mean, inplace=True)
+  return data
+
+def preprocess_data(df, label_mapping):
+    df = df.copy()
+
+    df['label'] = df['label'].replace(label_mapping)
+
+    y = df['label'].copy()
+    X = df.drop('label', axis=1).copy()
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state=123)
+
+    return X_train, X_test, y_train, y_test
